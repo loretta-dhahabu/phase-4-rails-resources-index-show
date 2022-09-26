d121b8be29c7c8da616b6bf5349536e657029144
loretta dhahabu <loreydjefwa@gmail.com>
2022-09-26 11:31:31 +0300

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # #GET/birds => show a list of all birds
+  # get '/birds', to: 'birds#index'
+  
+  # #GET /birds/:id  => show one specific bird
+  # get '/birds/:id', to: 'birds#show'
+
+  #use "resources" tool
+  resources :birds, only: [:index, :show]
 end
